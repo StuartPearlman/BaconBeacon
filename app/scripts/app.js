1198b07abe9b93e68cb0da9130014522e742135e
Stuart Pearlman <stupearlman@gmail.com>
2015-09-05 21:07:20 -0500

    Hook up google maps

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -9,27 +9,36 @@
  * Main module of the application.
  */
 angular
-  .module('googleMusicMakerApp', [
-    'ngAnimate',
-    'ngAria',
-    'ngCookies',
-    'ngMessages',
-    'ngResource',
-    'ngRoute',
-    'ngSanitize',
-    'ngTouch'
-  ])
-  .config(function ($routeProvider) {
-    $routeProvider
-      .when('/', {
-        templateUrl: 'views/main.html',
-        controller: 'MainCtrl'
-      })
-      .when('/about', {
-        templateUrl: 'views/about.html',
-        controller: 'AboutCtrl'
-      })
-      .otherwise({
-        redirectTo: '/'
-      });
+
+.module('googleMusicMakerApp', [
+  'ngAnimate',
+  'ngAria',
+  'ngCookies',
+  'ngMessages',
+  'ngResource',
+  'ngRoute',
+  'ngSanitize',
+  'ngTouch',
+
+  'uiGmapgoogle-maps'
+])
+.config(function ($routeProvider, uiGmapGoogleMapApiProvider, API_KEYS) {
+  $routeProvider
+    .when('/', {
+      templateUrl: 'views/main.html',
+      controller: 'MainCtrl'
+    })
+    .when('/about', {
+      templateUrl: 'views/about.html',
+      controller: 'AboutCtrl'
+    })
+    .otherwise({
+      redirectTo: '/'
+    });
+
+  uiGmapGoogleMapApiProvider.configure({
+    key: API_KEYS.googleMaps,
+    v: '3.20', //defaults to latest 3.X anyhow
+    libraries: 'weather,geometry,visualization'
   });
+});
